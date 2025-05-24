aa6383fc80a57ba66916e64fd6d341aabf216b5c
SAM <michaelslocalserver@hotmail.com>
2025-05-23 21:00:13 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
@@ -66,13 +67,16 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
 
-    // Compose BOM
+    // Compose BOM - IMPORTANT: This manages all Compose versions
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    
+    // Animation
+    implementation("androidx.compose.animation:animation")
 
     // ViewModel and State
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
